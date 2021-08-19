8189e82b67e26a5bbad0c19ff83cd4f8148bf4fd
Joao Pedro <joaopedroalb.dev@gmail.com>
2021-08-19 15:22:43 -0300

    Finalizando a parte do dark e white mode

--- src/Components/FooterMobile/index.tsx
+++ src/Components/FooterMobile/index.tsx
@@ -3,10 +3,10 @@ import styles from './index.module.scss'
 import {useTask } from '../../contexts/TaskContext'
 
 export default function FooterMobile(props:{filter:number,setFilter(int:number):any}){
-    const {taskLeft,clearTaskFinished } = useTask();
+    const {isDark } = useTask();
 
     return(
-        <div className={styles.filterContainer}>
+        <div className={isDark ? styles.filterContainerDark:styles.filterContainerWhite}>
             <label onClick={()=>props.setFilter(0)} className={props.filter==0?styles.selectedFilter:""}>All</label>
             <label onClick={()=>props.setFilter(1)} className={props.filter==1?styles.selectedFilter:""}>Active</label>
             <label onClick={()=>props.setFilter(2)} className={props.filter==2?styles.selectedFilter:""}>Completed</label>
